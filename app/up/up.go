youcaodian2/telegram-download

feat(upload): support `--include` flag

app/up/up.go
@@ -25,13 +25,14 @@ import (
 type Options struct {
 	Chat     string
 	Paths    []string
+	Includes []string
 	Excludes []string
 	Remove   bool
 	Photo    bool
 }
 
 func Run(ctx context.Context, c *telegram.Client, kvd kv.KV, opts Options) (rerr error) {
-	files, err := walk(opts.Paths, opts.Excludes)
+	files, err := walk(opts.Paths, opts.Includes, opts.Excludes)
 	if err != nil {
 		return err
 	}

XMLHexagram/tdl

app/up/up.go
@@ -30,6 +30,7 @@ type Options struct {
 	Thread   int
 	To       string
 	Paths    []string
+	Includes []string
 	Excludes []string
 	Remove   bool
 	Photo    bool
@@ -48,7 +49,7 @@ func Run(ctx context.Context, c *telegram.Client, kvd storage.Storage, opts Opti
 		return nil
 	}
 
-	files, err := walk(opts.Paths, opts.Excludes)
+	files, err := walk(opts.Paths, opts.Excludes, opts.Includes)
 	if err != nil {
 		return err
 	}